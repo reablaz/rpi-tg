6948aa03695f3ef217379682a755360d53595bf3
edgarsb <b@ysx.in>
2019-01-18 16:11:08 +0200

    added video

--- tg.py
+++ tg.py
@@ -25,6 +25,7 @@ class DetectMotion(array.PiMotionAnalysis):
             np.square(a['x'].astype(np.float)) +
             np.square(a['y'].astype(np.float))
             ).clip(0, 255).astype(np.uint8)
+        print(a)
         # If there're more than 10 vectors with a magnitude greater
         # than 60, then say we've detected motion
         if (a > 80).sum() > 50:
